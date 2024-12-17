6d5e4158a930799deb29fc81e93f477c4327c9a3
Adrian Tosca <atosca@adobe.com>
2024-12-17 12:46:55 +0200

    day 15 part 2

--- src/main/kotlin/earth/adi/aoc2024/Day15.kt
+++ src/main/kotlin/earth/adi/aoc2024/Day15.kt
@@ -1,15 +1,29 @@
 package earth.adi.aoc2024
 
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.BOX
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.EMPTY
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.ROBOT
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.WALL
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.WIDE_BOX_H1
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.WIDE_BOX_H2
+
 class Day15 {
 
   data class WarehouseMap(val data: MutableList<MutableList<Char>>) {
     fun at(p: Position): Char {
+      if (p.x < 0 || p.x >= width || p.y < 0 || p.y >= height) return WALL
       return data[p.y][p.x]
     }
 
     fun set(p: Position, c: Char) {
       data[p.y][p.x] = c
     }
+
+    val width: Int
+      get() = data[0].size
+
+    val height: Int
+      get() = data.size
 
     fun find(e: Char): Position {
       return data
@@ -19,10 +33,58 @@ class Day15 {
           .first()
     }
 
-    fun move(fromPos: Position, move: Move): Boolean {
+    fun print() {
+      data.forEach { row -> println(row.joinToString("")) }
+      println()
+    }
+
+    fun widen(): WarehouseMap {
+      val newData = mutableListOf<MutableList<Char>>()
+      for (y in 0 until height) {
+        val newRow = mutableListOf<Char>()
+        for (x in 0 until width) {
+          val cell = at(Position(x, y))
+          if (cell == BOX) {
+            newRow.add(WIDE_BOX_H1)
+            newRow.add(WIDE_BOX_H2)
+          } else if (cell == ROBOT) {
+            newRow.add(ROBOT)
+            newRow.add(EMPTY)
+          } else {
+            newRow.add(cell)
+            newRow.add(cell)
+          }
+        }
+        newData.add(newRow)
+      }
+      return WarehouseMap(newData)
+    }
+
+    companion object {
+      const val ROBOT = '@'
+      const val BOX = 'O'
+      const val WALL = '#'
+      const val EMPTY = '.'
+      const val WIDE_BOX_H1 = '['
+      const val WIDE_BOX_H2 = ']'
+
+      fun from(input: List<String>): WarehouseMap {
+        return WarehouseMap(input.map { it.toMutableList() }.toMutableList())
+      }
+    }
+  }
+
+  interface MoveController {
+    fun move(fromPos: Position, move: Move): Boolean
+
+    fun boxPositions(): List<Position>
+  }
+
+  class NarrowController(val map: WarehouseMap) : MoveController {
+    override fun move(fromPos: Position, move: Move): Boolean {
       val nextPos = fromPos.movedBy(move)
-      if (at(nextPos) == Warehouse.WALL) return false
-      if (at(nextPos) == Warehouse.BOX) {
+      if (map.at(nextPos) == WALL) return false
+      if (map.at(nextPos) == BOX) {
         if (move(nextPos, move)) {
           swap(fromPos, nextPos)
           return true
@@ -34,45 +96,137 @@ class Day15 {
       return true
     }
 
-    fun boxPositions(): List<Position> {
-      return data.flatMapIndexed { y, row ->
-        row.mapIndexedNotNull { x, cell -> if (cell == Warehouse.BOX) Position(x, y) else null }
+    override fun boxPositions(): List<Position> {
+      return map.data.flatMapIndexed { y, row ->
+        row.mapIndexedNotNull { x, cell -> if (cell == BOX) Position(x, y) else null }
       }
     }
 
     fun swap(p1: Position, p2: Position) {
-      val temp = at(p1)
-      set(p1, at(p2))
-      set(p2, temp)
-    }
-
-    fun print() {
-      data.forEach { row -> println(row.joinToString("")) }
-      println()
-    }
-
-    companion object {
-      fun from(input: List<String>): WarehouseMap {
-        return WarehouseMap(input.map { it.toMutableList() }.toMutableList())
-      }
+      val temp = map.at(p1)
+      map.set(p1, map.at(p2))
+      map.set(p2, temp)
     }
   }
 
-  data class Warehouse(val map: WarehouseMap, val moves: List<Move>, var robotPosition: Position) {
+  class WideController(val map: WarehouseMap) : MoveController {
+    override fun move(fromPos: Position, move: Move): Boolean {
+      if (map.at(fromPos) == EMPTY) return true
+      if (!canMove(fromPos, move)) {
+        return false
+      }
+      if (WideBox.isWideBox(map.at(fromPos))) {
+        val wideBoxPos = locateWideBox(fromPos)
+        val nextWideBoxPos = wideBoxPos.movedBy(move)
+        if (move == Move.LEFT) {
+          if (WideBox.isWideBox(map.at(nextWideBoxPos.h1))) {
+            move(nextWideBoxPos.h1, move)
+          }
+          swap(wideBoxPos.h1, nextWideBoxPos.h1)
+          swap(wideBoxPos.h2, nextWideBoxPos.h2)
+        } else if (move == Move.RIGHT) {
+          if (WideBox.isWideBox(map.at(nextWideBoxPos.h2))) {
+            move(nextWideBoxPos.h2, move)
+          }
+          swap(wideBoxPos.h2, nextWideBoxPos.h2)
+          swap(wideBoxPos.h1, nextWideBoxPos.h1)
+        } else {
+          if (WideBox.isWideBox(map.at(nextWideBoxPos.h1)) ||
+              WideBox.isWideBox(map.at(nextWideBoxPos.h2))) {
+            move(nextWideBoxPos.h1, move)
+            move(nextWideBoxPos.h2, move)
+          }
+          swap(wideBoxPos.h1, nextWideBoxPos.h1)
+          swap(wideBoxPos.h2, nextWideBoxPos.h2)
+        }
+      } else {
+        val nextPos = fromPos.movedBy(move)
+        move(nextPos, move)
+        swap(fromPos, nextPos)
+      }
+      return true
+    }
 
-    fun moveRobot() {
-      moves.forEach { move ->
-        if (map.move(robotPosition, move)) {
-          robotPosition = robotPosition.movedBy(move)
+    fun canMove(fromPos: Position, move: Move): Boolean {
+      val cell = map.at(fromPos)
+      if (cell == EMPTY) return true
+      if (cell == WALL) return false
+      if (WideBox.isWideBox(cell)) {
+        val wideBoxPos = locateWideBox(fromPos)
+        val nextWideBoxPos = wideBoxPos.movedBy(move)
+        return if (move == Move.LEFT) {
+          canMove(nextWideBoxPos.h1, move)
+        } else if (move == Move.RIGHT) {
+          canMove(nextWideBoxPos.h2, move)
+        } else {
+          canMove(nextWideBoxPos.h1, move) && canMove(nextWideBoxPos.h2, move)
+        }
+      }
+      return canMove(fromPos.movedBy(move), move)
+    }
+
+    fun locateWideBox(p: Position): WideBox {
+      return if (map.at(p) == WIDE_BOX_H1) {
+        WideBox(p)
+      } else {
+        WideBox(p.movedBy(Move.LEFT))
+      }
+    }
+
+    data class WideBox(val h1: Position, val h2: Position = h1.movedBy(Move.RIGHT)) {
+      fun movedBy(move: Move): WideBox {
+        return WideBox(h1.movedBy(move), h2.movedBy(move))
+      }
+
+      companion object {
+        fun isWideBox(char: Char): Boolean {
+          return char == WIDE_BOX_H1 || char == WIDE_BOX_H2
         }
       }
     }
 
+    override fun boxPositions(): List<Position> {
+      return map.data.flatMapIndexed { y, row ->
+        row.mapIndexedNotNull { x, cell -> if (cell == WIDE_BOX_H1) Position(x, y) else null }
+      }
+    }
+
+    fun swap(p1: Position, p2: Position) {
+      val temp = map.at(p1)
+      map.set(p1, map.at(p2))
+      map.set(p2, temp)
+    }
+  }
+
+  data class Warehouse(
+      val map: WarehouseMap,
+      val moves: List<Move>,
+      var robotPosition: Position,
+      val controller: MoveController = NarrowController(map),
+  ) {
+
+    fun withWidenMap(): Warehouse {
+      val widenMap = map.widen()
+      return Warehouse(widenMap, moves, widenMap.find(ROBOT), WideController(widenMap))
+    }
+
+    fun moveRobot() {
+      moves.forEach { move -> moveRobot(move) }
+    }
+
+    fun moveRobot(move: Move) {
+      //      println(move.char)
+      if (controller.move(robotPosition, move)) {
+        robotPosition = robotPosition.movedBy(move)
+      }
+      //      map.print()
+    }
+
+    fun sumOfGps(): Int {
+      return controller.boxPositions().sumOf { it.gps }
+    }
+
     companion object {
-      const val ROBOT = '@'
-      const val BOX = 'O'
-      const val WALL = '#'
-      const val EMPTY = '.'
 
       fun from(input: String): Warehouse {
         val warehouseText = input.lines().takeWhile { it.isNotEmpty() }
@@ -89,18 +243,6 @@ class Day15 {
     fun movedBy(move: Move): Position {
       return Position(x + move.dx, y + move.dy)
     }
-
-    val up: Position
-      get() = Position(x, y - 1)
-
-    val right: Position
-      get() = Position(x + 1, y)
-
-    val down: Position
-      get() = Position(x, y + 1)
-
-    val left: Position
-      get() = Position(x - 1, y)
 
     val gps: Int
       get() = y * 100 + x
@@ -127,14 +269,16 @@ class Day15 {
     fun part1(inputResourceName: String): Int {
       val warehouse = input(inputResourceName)
       warehouse.moveRobot()
-      val result = warehouse.map.boxPositions().sumOf { it.gps }
+      val result = warehouse.sumOfGps()
       println("Day15 part1 = $result")
       return result
     }
 
     fun part2(inputResourceName: String): Int {
-      val robots = input(inputResourceName)
-      val result = 0
+      val narrowWarehouse = input(inputResourceName)
+      val warehouse = narrowWarehouse.withWidenMap()
+      warehouse.moveRobot()
+      val result = warehouse.sumOfGps()
       println("Day15 part2 = $result")
       return result
     }
--- src/test/kotlin/earth/adi/aoc2024/Day15KtTest.kt
+++ src/test/kotlin/earth/adi/aoc2024/Day15KtTest.kt
@@ -1,6 +1,8 @@
 package earth.adi.aoc2024
 
 import earth.adi.aoc2024.Day15.Companion.INPUT_DAY_15
+import earth.adi.aoc2024.Day15.Move
+import earth.adi.aoc2024.Day15.WarehouseMap.Companion.ROBOT
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -8,6 +10,7 @@ class Day15KtTest {
   companion object {
     private const val INPUT_DAY_15_TEST = "/day_15_test.txt"
     private const val INPUT_DAY_15_TEST_B = "/day_15_test_b.txt"
+    private const val INPUT_DAY_15_TEST_C = "/day_15_test_c.txt"
   }
 
   @Test
@@ -70,7 +73,126 @@ class Day15KtTest {
   }
 
   @Test
+  fun part1_widen() {
+    val warehouse = Day15.input(INPUT_DAY_15_TEST)
+    warehouse.map.print()
+    val widenMap = warehouse.withWidenMap().map
+    widenMap.print()
+    assertThat(widenMap)
+        .isEqualTo(
+            Day15.WarehouseMap.from(
+                """
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##....[]@.....[]..##
+        ##[]##....[]......##
+        ##[]....[]....[]..##
+        ##..[][]..[]..[][]##
+        ##........[]......##
+        ####################
+        """
+                    .trimIndent()
+                    .lines()))
+  }
+
+  @Test
+  fun part2_moveSteps() {
+    val narrowWarehouse = Day15.input(INPUT_DAY_15_TEST_C)
+    val warehouse = narrowWarehouse.withWidenMap()
+    for (move in warehouse.moves) {
+      println(move.char)
+      warehouse.moveRobot(move)
+      warehouse.map.print()
+    }
+  }
+
+  @Test
+  fun part2_moveOne() {
+    val narrowWarehouse = Day15.input(INPUT_DAY_15_TEST_C)
+    val warehouse = narrowWarehouse.withWidenMap()
+    warehouse.map.print()
+    for (i in 0 until 5) {
+      val move = warehouse.moves[i]
+      println(move.char)
+      warehouse.moveRobot(move)
+      warehouse.map.print()
+    }
+    println(Move.UP.char)
+    warehouse.moveRobot(Move.UP)
+    warehouse.map.print()
+  }
+
+  @Test
+  fun part2_moveTricky1() {
+    val map =
+        Day15.WarehouseMap.from(
+            """
+        ##############
+        ##..[]......##
+        ##[]........##
+        ##.[].......##
+        ##.@........##
+        ##..........##
+        ##############
+        """
+                .trimIndent()
+                .lines())
+    val warehouse =
+        Day15.Warehouse(map, listOf(Move.UP), map.find(ROBOT), Day15.WideController(map))
+    warehouse.map.print()
+    warehouse.moveRobot(Move.UP)
+    warehouse.map.print()
+  }
+
+  @Test
+  fun part2_moveTricky2() {
+    val map =
+        Day15.WarehouseMap.from(
+            """
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##...[]...[]..[]..##
+        ##[]##....[]......##
+        ##[][]........[]..##
+        ##....@[]..[].[][]##
+        ##........[]......##
+        ####################
+        """
+                .trimIndent()
+                .lines())
+    val warehouse =
+        Day15.Warehouse(map, listOf(Move.UP), map.find(ROBOT), Day15.WideController(map))
+    warehouse.map.print()
+    warehouse.moveRobot(Move.UP)
+  }
+
+  @Test
+  fun part2_move() {
+    val narrowWarehouse = Day15.input(INPUT_DAY_15_TEST_C)
+    val warehouse = narrowWarehouse.withWidenMap()
+    warehouse.moveRobot()
+    assertThat(warehouse.map)
+        .isEqualTo(
+            Day15.WarehouseMap.from(
+                """
+        ##############
+        ##...[].##..##
+        ##...@.[]...##
+        ##....[]....##
+        ##..........##
+        ##..........##
+        ##############
+        """
+                    .trimIndent()
+                    .lines()))
+  }
+
+  @Test
   fun part2_test() {
-    assertThat(Day15.part2(INPUT_DAY_15_TEST)).isEqualTo(-1)
+    assertThat(Day15.part2(INPUT_DAY_15_TEST)).isEqualTo(9021)
   }
 }
